4676c8064f36c5d8052c151f6b6df7b99994ef6b
AIPC <grillnov@gmail.com>
2017-11-16 00:55:38 -0500

    Done chunk defining

--- assignment_package/src/scene/terrain.cpp
+++ assignment_package/src/scene/terrain.cpp
@@ -57,3 +57,589 @@ void Terrain::CreateTestScene()
         m_blocks[32][y][32] = GRASS;
     }
 }
+
+
+Chunk::Chunk(OpenGLContext *parent, uint64_t xz) : Drawable(parent), xzGlobalPos(xz),
+    adjPosZ(nullptr), adjNegZ(nullptr), adjPosX(nullptr), adjNegX(nullptr)
+{
+}
+
+BlockType Chunk::getBlockType(size_t x, size_t y, size_t z) const
+{
+    return this->blocks[4096*z + 16*y + x];
+}
+
+BlockType& Chunk::accessBlockType(size_t x, size_t y, size_t z)
+{
+    return this->blocks[4096*z + 16*y + x];
+}
+
+void Chunk::setAdjPosZ(Chunk *ptr)
+{
+    this->adjPosZ = ptr;
+}
+
+void Chunk::setAdjNegZ(Chunk *ptr)
+{
+    this->adjNegZ = ptr;
+}
+
+void Chunk::setAdjPosX(Chunk *ptr)
+{
+    this->adjPosX = ptr;
+}
+
+void Chunk::setAdjNegX(Chunk *ptr)
+{
+    this->adjNegX = ptr;
+}
+
+static const GLfloat dirtColor[4] = {0.475f, 0.333f, 0.227f, 1.0f};
+static const GLfloat grassColor[4] = {0.373f, 0.624f, 0.208f, 1.0f};
+static const GLfloat stoneColor[4] = {0.5f, 0.5f, 0.5f, 1.0f};
+const GLfloat cubeRadius = 1.0f;
+
+void Chunk::fillLeftFace(size_t x, size_t y, size_t z, BlockType type)
+{
+    glm::vec4 square[4] =
+    {
+        glm::vec4(x, y, z, 1.0f),
+        glm::vec4(x, y, z + cubeRadius, 1.0f),
+        glm::vec4(x, y + cubeRadius, z + cubeRadius, 1.0f),
+        glm::vec4(x, y + cubeRadius, z, 1.0f)
+    };
+    glm::vec4 offset = glm::vec4(this->xzGlobalPos >> 32, 0.0f, this->xzGlobalPos & 0xFFFFFFFF, 1.0f);
+
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        square[i] += offset;
+        for (unsigned j = 0; j!=4; ++j)
+        {
+            pos.push_back(square[i][j]);
+        }
+    }
+
+    glm::vec4 normal = glm::vec4(-1.0f, 0.0f, 0.0f, 1.0f);
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        nor.push_back(normal[i]);
+    }
+
+    switch (type)
+    {
+    case GRASS:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(grassColor[j]);
+            }
+        }
+        break;
+    case DIRT:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(dirtColor[j]);
+            }
+        }
+        break;
+    case STONE:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(stoneColor[j]);
+            }
+        }
+        break;
+    default:
+        break;
+    }
+
+    size_t indexoffset = ele.size() / 3 * 2;
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 1);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset + 3);
+}
+
+void Chunk::fillRightFace(size_t x, size_t y, size_t z, BlockType type)
+{
+    glm::vec4 square[4] =
+    {
+        glm::vec4(x + cubeRadius, y, z, 1.0f),
+        glm::vec4(x + cubeRadius, y + cubeRadius, z, 1.0f),
+        glm::vec4(x + cubeRadius, y + cubeRadius, z + cubeRadius, 1.0f),
+        glm::vec4(x + cubeRadius, y, z + cubeRadius, 1.0f)
+    };
+    glm::vec4 offset = glm::vec4(this->xzGlobalPos >> 32, 0.0f, this->xzGlobalPos & 0xFFFFFFFF, 1.0f);
+
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        square[i] += offset;
+        for (unsigned j = 0; j!=4; ++j)
+        {
+            pos.push_back(square[i][j]);
+        }
+    }
+
+    glm::vec4 normal = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        nor.push_back(normal[i]);
+    }
+
+    switch (type)
+    {
+    case GRASS:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(grassColor[j]);
+            }
+        }
+        break;
+    case DIRT:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(dirtColor[j]);
+            }
+        }
+        break;
+    case STONE:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(stoneColor[j]);
+            }
+        }
+        break;
+    default:
+        break;
+    }
+
+    size_t indexoffset = ele.size() / 3 * 2;
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 1);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset + 3);
+}
+
+void Chunk::fillUpFace(size_t x, size_t y, size_t z, BlockType type)
+{
+    glm::vec4 square[4] =
+    {
+        glm::vec4(x, y + cubeRadius, z, 1.0f),
+        glm::vec4(x, y + cubeRadius, z + cubeRadius, 1.0f),
+        glm::vec4(x + cubeRadius, y + cubeRadius, z + cubeRadius, 1.0f),
+        glm::vec4(x + cubeRadius, y + cubeRadius, z, 1.0f)
+    };
+    glm::vec4 offset = glm::vec4(this->xzGlobalPos >> 32, 0.0f, this->xzGlobalPos & 0xFFFFFFFF, 1.0f);
+
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        square[i] += offset;
+        for (unsigned j = 0; j!=4; ++j)
+        {
+            pos.push_back(square[i][j]);
+        }
+    }
+
+    glm::vec4 normal = glm::vec4(0.0f, 1.0f, 0.0f, 1.0f);
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        nor.push_back(normal[i]);
+    }
+
+    switch (type)
+    {
+    case GRASS:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(grassColor[j]);
+            }
+        }
+        break;
+    case DIRT:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(dirtColor[j]);
+            }
+        }
+        break;
+    case STONE:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(stoneColor[j]);
+            }
+        }
+        break;
+    default:
+        break;
+    }
+
+    size_t indexoffset = ele.size() / 3 * 2;
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 1);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset + 3);
+}
+
+void Chunk::fillDownFace(size_t x, size_t y, size_t z, BlockType type)
+{
+    glm::vec4 square[4] =
+    {
+        glm::vec4(x, y, z, 1.0f),
+        glm::vec4(x + cubeRadius, y, z, 1.0f),
+        glm::vec4(x + cubeRadius, y, z + cubeRadius, 1.0f),
+        glm::vec4(x, y, z + cubeRadius, 1.0f)
+    };
+    glm::vec4 offset = glm::vec4(this->xzGlobalPos >> 32, 0.0f, this->xzGlobalPos & 0xFFFFFFFF, 1.0f);
+
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        square[i] += offset;
+        for (unsigned j = 0; j!=4; ++j)
+        {
+            pos.push_back(square[i][j]);
+        }
+    }
+
+    glm::vec4 normal = glm::vec4(0.0f, -1.0f, 0.0f, 1.0f);
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        nor.push_back(normal[i]);
+    }
+
+    switch (type)
+    {
+    case GRASS:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(grassColor[j]);
+            }
+        }
+        break;
+    case DIRT:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(dirtColor[j]);
+            }
+        }
+        break;
+    case STONE:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(stoneColor[j]);
+            }
+        }
+        break;
+    default:
+        break;
+    }
+
+    size_t indexoffset = ele.size() / 3 * 2;
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 1);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset + 3);
+}
+
+void Chunk::fillFrontFace(size_t x, size_t y, size_t z, BlockType type)
+{
+    glm::vec4 square[4] =
+    {
+        glm::vec4(x, y, z + cubeRadius, 1.0f),
+        glm::vec4(x + cubeRadius, y, z + cubeRadius, 1.0f),
+        glm::vec4(x + cubeRadius, y + cubeRadius, z + cubeRadius, 1.0f),
+        glm::vec4(x, y + cubeRadius, z + cubeRadius, 1.0f)
+    };
+    glm::vec4 offset = glm::vec4(this->xzGlobalPos >> 32, 0.0f, this->xzGlobalPos & 0xFFFFFFFF, 1.0f);
+
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        square[i] += offset;
+        for (unsigned j = 0; j!=4; ++j)
+        {
+            pos.push_back(square[i][j]);
+        }
+    }
+
+    glm::vec4 normal = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        nor.push_back(normal[i]);
+    }
+
+    switch (type)
+    {
+    case GRASS:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(grassColor[j]);
+            }
+        }
+        break;
+    case DIRT:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(dirtColor[j]);
+            }
+        }
+        break;
+    case STONE:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(stoneColor[j]);
+            }
+        }
+        break;
+    default:
+        break;
+    }
+
+    size_t indexoffset = ele.size() / 3 * 2;
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 1);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset + 3);
+}
+
+void Chunk::fillBackFace(size_t x, size_t y, size_t z, BlockType type)
+{
+    glm::vec4 square[4] =
+    {
+        glm::vec4(x, y, z, 1.0f),
+        glm::vec4(x, y + cubeRadius, z, 1.0f),
+        glm::vec4(x + cubeRadius, y + cubeRadius, z, 1.0f),
+        glm::vec4(x + cubeRadius, y, z, 1.0f)
+    };
+    glm::vec4 offset = glm::vec4(this->xzGlobalPos >> 32, 0.0f, this->xzGlobalPos & 0xFFFFFFFF, 1.0f);
+
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        square[i] += offset;
+        for (unsigned j = 0; j!=4; ++j)
+        {
+            pos.push_back(square[i][j]);
+        }
+    }
+
+    glm::vec4 normal = glm::vec4(0.0f, 0.0f, -1.0f, 1.0f);
+    for (unsigned i = 0; i!=4; ++i)
+    {
+        nor.push_back(normal[i]);
+    }
+
+    switch (type)
+    {
+    case GRASS:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(grassColor[j]);
+            }
+        }
+        break;
+    case DIRT:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(dirtColor[j]);
+            }
+        }
+        break;
+    case STONE:
+        for (unsigned i = 0; i!=4; ++i)
+        {
+            for (unsigned j = 0; j!=4; ++j)
+            {
+                col.push_back(stoneColor[j]);
+            }
+        }
+        break;
+    default:
+        break;
+    }
+
+    size_t indexoffset = ele.size() / 3 * 2;
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 1);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset);
+    ele.push_back(indexoffset + 2);
+    ele.push_back(indexoffset + 3);
+}
+
+void Chunk::create()
+{
+    std::vector<GLfloat> unpackedPosition;
+    std::vector<GLfloat> unpackedNormal;
+    std::vector<GLfloat> unpackedColor;
+    std::vector<GLuint> unpackedElement;
+
+    for (size_t x = 0; x != 16; ++x)
+    {
+        for (size_t y = 0; y != 256; ++y)
+        {
+            for (size_t z = 0; z != 16; ++z)
+            {
+                if (this->getBlockType(x, y, z) == EMPTY)
+                {
+                    continue;
+                }
+
+                //Check left
+                if (x == 0)
+                {
+                    //Check the chunk to its left
+                    if (this->adjNegX == nullptr ||
+                            this->adjNegX->getBlockType(15, y, z) == EMPTY)
+                    {
+                        fillLeftFace(x, y, z, this->getBlockType(x, y, z));
+                    }
+                }
+                //Check the block to its left
+                else
+                {
+                    if (this->getBlockType(x - 1, y, z) == EMPTY)
+                    {
+                        fillLeftFace(x, y, z, this->getBlockType(x, y, z));
+                    }
+                }
+
+
+                //Check right
+                if (x == 15)
+                {
+                    //Checkthe chunk to its right
+                    if (this->adjPosX == nullptr ||
+                            this->adjPosX->getBlockType(0, y, z) == EMPTY)
+                    {
+                        fillRightFace(x, y, z, this->getBlockType(x, y, z));
+                    }
+                }
+                //Check the block to its right
+                else
+                {
+                    if (this->getBlockType(x + 1, y, z) == EMPTY)
+                    {
+                        fillRightFace(x, y, z, this->getBlockType(x, y, z));
+                    }
+                }
+
+
+                //Check back
+                if (z == 0)
+                {
+                    //Check the chunk to its back
+                    if (this->adjNegZ == nullptr ||
+                            this->adjNegZ->getBlockType(x, y, 15) == EMPTY)
+                    {
+                        fillBackFace(x, y, z, this->getBlockType(x, y, z));
+                    }
+                }
+                //Check the block to its back
+                else
+                {
+                    if (this->getBlockType(x, y, z - 1) == EMPTY)
+                    {
+                        fillBackFace(x, y, z, this->getBlockType(x, y, z));
+                    }
+                }
+
+
+                //Check front
+                if (z == 15)
+                {
+                    //Check the chunk to its front
+                    if (this->adjPosZ == nullptr ||
+                            this->adjPosZ->getBlockType(x, y, 0) == EMPTY)
+                    {
+                        fillFrontFace(x, y, z, this->getBlockType(x, y, z));
+                    }
+                }
+                //Check the block to its front
+                else
+                {
+                    if (this->getBlockType(x, y, z + 1) == EMPTY)
+                    {
+                        fillFrontFace(x, y, z, this->getBlockType(x, y, z));
+                    }
+                }
+
+                //Check up
+                //Check the block above it
+                if (y == 255 || this->getBlockType(x, y + 1, z) == EMPTY)
+                {
+                    fillUpFace(x, y, z, this->getBlockType(x, y, z));
+                }
+
+                //Check down
+                //Check the block below it
+                if (y == 0 || this->getBlockType(x, y - 1, z) == EMPTY)
+                {
+                    fillDownFace(x, y, z, this->getBlockType(x, y, z));
+                }
+            }
+        }
+    }
+
+    this->count = unpackedElement.size();
+
+    generateIdx();
+    context->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bufIdx);
+    context->glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * ele.size(),
+                             reinterpret_cast<void*>(ele.data()), GL_STATIC_DRAW);
+
+    generatePos();
+    context->glBindBuffer(GL_ARRAY_BUFFER, bufPos);
+    context->glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * pos.size(),
+                             reinterpret_cast<void*>(pos.data()), GL_STATIC_DRAW);
+
+    generateNor();
+    context->glBindBuffer(GL_ARRAY_BUFFER, bufNor);
+    context->glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * nor.size(),
+                             reinterpret_cast<void*>(nor.data()), GL_STATIC_DRAW);
+
+    generateCol();
+    context->glBindBuffer(GL_ARRAY_BUFFER, bufCol);
+    context->glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * col.size(),
+                             reinterpret_cast<void*>(col.data()), GL_STATIC_DRAW);
+}
--- assignment_package/src/scene/terrain.h
+++ assignment_package/src/scene/terrain.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <QList>
 #include <la.h>
+#include "drawable.h"
+#include <stdint.h>
 
 // C++ 11 allows us to define the size of an enum. This lets us use only one byte
 // of memory to store our different block types. By default, the size of a C++ enum
@@ -27,4 +29,39 @@ public:
     void setBlockAt(int x, int y, int z, BlockType t); // Given a world-space coordinate (which may have negative
                                                            // values) set the block at that point in space to the
                                                            // given type.
+};
+
+class Chunk : public Drawable
+{
+private:
+    BlockType blocks[65536];
+
+    uint64_t xzGlobalPos;
+    Chunk* adjPosZ;
+    Chunk* adjNegZ;
+    Chunk* adjPosX;
+    Chunk* adjNegX;
+
+    std::vector<GLfloat> pos;
+    std::vector<GLfloat> nor;
+    std::vector<GLfloat> col;
+    std::vector<GLuint> ele;
+public:
+    Chunk(OpenGLContext* parent, uint64_t xz);
+    BlockType getBlockType(size_t x, size_t y, size_t z) const;
+    BlockType& accessBlockType(size_t x, size_t y, size_t z);
+
+    void setAdjPosZ(Chunk* ptr);
+    void setAdjNegZ(Chunk* ptr);
+    void setAdjPosX(Chunk* ptr);
+    void setAdjNegX(Chunk* ptr);
+
+    void fillLeftFace(size_t x, size_t y, size_t z, BlockType type);
+    void fillRightFace(size_t x, size_t y, size_t z, BlockType type);
+    void fillFrontFace(size_t x, size_t y, size_t z, BlockType type);
+    void fillBackFace(size_t x, size_t y, size_t z, BlockType type);
+    void fillUpFace(size_t x, size_t y, size_t z, BlockType type);
+    void fillDownFace(size_t x, size_t y, size_t z, BlockType type);
+
+    virtual void create() override;
 };
